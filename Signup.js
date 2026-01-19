7a2beae54151e76b5e332a9f5b27d4fd24614797
Baji hussen <bajihussen143@gmail.com>
2026-01-19 11:23:32 +0530

    Signup.js

---
+++ Signup.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Account Created Successfully");
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <h2>Create Account ✨</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit">Sign Up</button>
+        </form>
+
+        <p>
+          Already have an account? <Link to="/">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
