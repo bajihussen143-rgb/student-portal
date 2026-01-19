cea5dd4a83aa8dd035dc39c40be568ed7cc48335
Baji hussen <bajihussen143@gmail.com>
2026-01-19 11:20:49 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import "./Auth.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
